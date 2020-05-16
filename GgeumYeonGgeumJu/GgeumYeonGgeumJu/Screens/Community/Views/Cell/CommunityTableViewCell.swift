dc29e3e15f136e079088cdf8bf5cd7ad7819b13c
namsoo5 <are34@naver.com>
2020-05-16 17:47:07 +0900

    UPDATE: 커뮤니티 틀디자인 구현

---
+++ GgeumYeonGgeumJu/GgeumYeonGgeumJu/Screens/Community/Views/Cell/CommunityTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommunityTableViewCell.swift
+//  GgeumYeonGgeumJu
+//
+//  Created by 남수김 on 2020/05/16.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import UIKit
+
+class CommunityTableViewCell: UITableViewCell {
+    static var reuseIdentifier = "CommunityCell"
+    static var nibName = "CommunityTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GgeumYeonGgeumJu/GgeumYeonGgeumJu/Screens/Community/Views/CommunityViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommunityViewController.swift
+//  GgeumYeonGgeumJu
+//
+//  Created by 남수김 on 2020/05/16.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import UIKit
+
+class CommunityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
