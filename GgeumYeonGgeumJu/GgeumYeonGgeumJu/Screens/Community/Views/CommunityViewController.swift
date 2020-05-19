0340baebec847c295260d323a4401138a569dfbc
namsoo5 <are34@naver.com>
2020-05-19 21:18:54 +0900

    UPDATE: 커뮤니티 tab 애니메이션구현, 스크롤 헤더구현

--- GgeumYeonGgeumJu/GgeumYeonGgeumJu/Screens/Community/Views/CommunityViewController.swift
+++ GgeumYeonGgeumJu/GgeumYeonGgeumJu/Screens/Community/Views/CommunityViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
+import SnapKit
 
 class CommunityViewController: UIViewController {
 
+    
+    @IBOutlet weak var tabUnderLineView: UIView!
+    @IBOutlet weak var headerViewTopLayout: NSLayoutConstraint!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerView: UIImageView!
+    
+    private let originHeaderHeight: CGFloat = 200 - 44
+    var tabUnderLayout: [NSLayoutConstraint] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func underBarAnimation(view: UIButton) {
+        tabUnderLineView.snp.removeConstraints()
+        tabUnderLineView.snp.makeConstraints {
+            $0.leading.equalTo(view.snp.leading)
+            $0.trailing.equalTo(view.snp.trailing)
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func recentOrderClick(_ sender: UIButton) {
+        underBarAnimation(view: sender)
+    }
+    
+    @IBAction func popularOrderClick(_ sender: UIButton) {
+        underBarAnimation(view: sender)
+    }
+    
+    @IBAction func myBoardClick(_ sender: UIButton) {
+        underBarAnimation(view: sender)
+    }
+}
+
+extension CommunityViewController: UITableViewDataSource ,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell(style: .default, reuseIdentifier: "")
         
     }
-
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let posY = scrollView.contentOffset.y > originHeaderHeight ?
+            originHeaderHeight : scrollView.contentOffset.y
+        headerViewTopLayout.constant = -posY
+    }
+    
 }
