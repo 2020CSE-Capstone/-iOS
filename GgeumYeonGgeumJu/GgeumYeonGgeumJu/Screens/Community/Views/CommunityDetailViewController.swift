340879145fe4bbd60538be87a905155f0a4122bd
namsoo5 <are34@naver.com>
2020-05-27 00:03:29 +0900

    UPDATE: 커뮤니티 상세내용UI 구현

---
+++ GgeumYeonGgeumJu/GgeumYeonGgeumJu/Screens/Community/Views/CommunityDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommunityDetailViewController.swift
+//  GgeumYeonGgeumJu
+//
+//  Created by 남수김 on 2020/05/27.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import UIKit
+
+class CommunityDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
